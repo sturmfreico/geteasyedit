54c11167010a62ac6947b614a65cde651620fc02
Christoph von Gellhorn <gellborn@gmail.com>
2025-07-02 13:22:56 +1000

    update tw config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {},
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      fontFamily: {
+        grotesk: ["Grotesk", "sans-serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+      },
+      colors: {
+        green: "var(--green)",
+        greenHover: "#7fe141",
+        darkGreen: "#173402",
+        dark: "var(--dark)",
+        gray: "var(--gray)",
+      },
+    },
+  },
   plugins: [],
 };
