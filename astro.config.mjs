0e4fbfda946b4114c648ba0ed92ccc2ffec52e56
Christoph von Gellhorn <gellborn@gmail.com>
2025-07-02 15:30:19 +1000

    upgrade tailwind and uplift styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
-  integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
